149abeb84a0b05f17cfeeddd381eaf47e4cc7206
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-04-30 16:44:00 +0200

    create component

---
+++ test-footer/src/components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+    <footer>
+      <div class="container">
+        <nav class="footer_nav">
+          <a class="footer__link" to="/about">Chi siamo</a>
+          <a class="footer__link" to="/contact">Contatti</a>
+          <a class="footer__link" to="/privacy">Privacy</a>
+        </nav>
+        <div class="footer__actions">
+          <button @click="handleClick" >Contattaci</button>
+        </div>
+      </div>
+    </footer>
+  </template>
+
+  <style scoped>
+  footer {
+    background-color: rgb(92, 107, 122);
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+  }
+  
+  .footer_nav {
+    margin-bottom: 10px;
+  }
+  
+  .footer__link {
+    color: #fff;
+    margin: 0 10px;
+    display: flex;
+    text-decoration: none;
+  }
+  
+  .footer__link:hover {
+    text-decoration: underline;
+  }
+  
+  .footer__actions {
+    margin-top: 20px;
+  }
+  
+  
+  
+  .footer__button:hover {
+    background-color: #0056b3;
+  }
+</style>
