ce651ed35f95a7c053fcecea6786c26282234d06
worldofvictory <133643234+worldofvictory@users.noreply.github.com>
2025-05-08 11:05:40 +0200

    test

--- test-footer/src/App.vue
+++ test-footer/src/App.vue
@@ -9,11 +9,20 @@ function showModal(): void {
   isShowModal.value=true
 }
 
+function seen(){
+  const seen="true"
+}
+function showM(): void {
+ seen.value=true
+}
+
 </script>
 
 <template>
   <main>
     <Header />
+    <p v-if="seen">Ora mi vedi</p>
+    <button @click="showM">ok</button>
     <h1>Hello</h1>
     <Footer @open-modal="showModal"/>
   <Modal v-if="isShowModal" @close="isShowModal = false" />  
